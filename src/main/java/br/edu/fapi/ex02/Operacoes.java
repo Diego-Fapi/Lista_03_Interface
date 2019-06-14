83c83639e5c9bfcddcb124722880d8645124e5d0
Diego <Diego@Diego-PC>
2019-06-13 23:57:53 -0300

    Exercicio Finalizado

---
+++ src/main/java/br/edu/fapi/ex02/Operacoes.java
@@ -0,0 +1,12 @@
+package br.edu.fapi.ex02;
+
+/**
+ *
+ * @author Diego
+ */
+public interface Operacoes {
+    double somar(double n1, double n2);
+    double subtrair(double n1, double n2);
+    double multiplicar(double n1, double n2);
+    double dividir(double n1, double n2);
+}
