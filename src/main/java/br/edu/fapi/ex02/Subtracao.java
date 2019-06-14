13be4cbaa25a09775b65eade61b44099541c116e
Diego <Diego@Diego-PC>
2019-06-13 23:58:07 -0300

    Exercicio Finalizado

---
+++ src/main/java/br/edu/fapi/ex02/Subtracao.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.fapi.ex02;
+
+/**
+ *
+ * @author Diego
+ */
+public class Subtracao implements Operacoes{
+    @Override
+    public double somar(double n1,double n2){
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double subtrair(double n1, double n2) {
+        return n1 - n2;
+    }
+
+    @Override
+    public double multiplicar(double n1, double n2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public double dividir(double n1, double n2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
